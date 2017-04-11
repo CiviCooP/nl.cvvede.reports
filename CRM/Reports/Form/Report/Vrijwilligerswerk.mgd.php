581b9022806ab5624643c2e00b8187534cb2d280
Jaap Jansma <jaap@edeveloper.nl>
2017-04-11 17:41:07 +0200

    added rapport voor vrijwilligerswerk

---
+++ CRM/Reports/Form/Report/Vrijwilligerswerk.mgd.php
@@ -0,0 +1,18 @@
+<?php
+
+return array (
+  0 =>
+    array (
+      'name' => 'CRM_Reports_Form_Report_Vrijwilligerswerk',
+      'entity' => 'ReportTemplate',
+      'params' =>
+        array (
+          'version' => 3,
+          'label' => 'Vrijwilligerswerk',
+          'description' => 'Vrijwilligerswerk (nl.cvvede.reports)',
+          'class_name' => 'CRM_Reports_Form_Report_Vrijwilligerswerk',
+          'report_url' => 'nl.cvvede.reports/vrijwilligerswerk',
+          'component' => 'CiviCase',
+        ),
+    ),
+);
